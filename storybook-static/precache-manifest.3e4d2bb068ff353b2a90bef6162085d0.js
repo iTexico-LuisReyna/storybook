464a73506df504b24b3450ed4cb2eb1e203874a1
Luis Reyna <luis.reyna@itexico.com>
2019-06-18 09:48:15 -0500

    debbing

---
+++ storybook-static/precache-manifest.3e4d2bb068ff353b2a90bef6162085d0.js
@@ -0,0 +1,35 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35993e614ddc8ea2613a4e1b6bee6273",
+    "url": "../build/lib/App.d.ts"
+  },
+  {
+    "revision": "780b296fad9ac72fe7590acb6f2e395b",
+    "url": "../build/lib/index.d.ts"
+  },
+  {
+    "revision": "2b5cd3609c8feef4a4a674c882dbd522",
+    "url": "../build/lib/serviceWorker.d.ts"
+  },
+  {
+    "revision": "4c2f900b01e6f6737c922f611b8d8cde",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.312d8efc65813d630a43.bundle.js"
+  },
+  {
+    "revision": "1833c15e293904b7378c",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.312d8efc65813d630a43.bundle.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "url": "vendors~main.312d8efc65813d630a43.bundle.js"
+  }
+]);
