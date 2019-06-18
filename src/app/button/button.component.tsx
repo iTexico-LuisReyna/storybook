c658271576537e8888bbdc56f738dd4c23eee0f3
Luis Reyna <luis.reyna@itexico.com>
2019-06-18 14:22:39 -0500

    [ADDONS] [ADD] Knobs and actions add-ons.

--- src/app/button/button.component.tsx
+++ src/app/button/button.component.tsx
@@ -1,27 +1,33 @@
-import React, { SFC } from 'react';
+import React from 'react';
 
 export interface IButtonProps {
   children?: React.ReactNode;
-  onClick?: (e: any) => void;
+  handleClick?: (e: any) => void;
+  disabled?: boolean;
+  color?: string;
 }
 
-const styles = {
-  border: '1px solid #eee',
-  borderRadius: 3,
-  backgroundColor: '#FFFFFF',
-  cursor: 'pointer',
-  fontSize: 15,
-  padding: '3px 10px',
-  margin: 10
-};
+export const ButtonComponent = ({ children, handleClick, disabled, color }: IButtonProps) => {
+  const styles = {
+    border: '1px solid #eee',
+    borderRadius: 3,
+    backgroundColor: '#FFFFFF',
+    cursor: 'pointer',
+    fontSize: 15,
+    padding: '3px 10px',
+    margin: 10,
+    color,
+  };
 
-export const ButtonComponent: SFC<IButtonProps> = props => (
-  <button onClick={props.onClick} style={styles} type="button">
-    {props.children}
-  </button>
-);
+  return (
+    <button onClick={handleClick} style={styles} type="button" disabled={disabled}>
+      {children}
+    </button>
+  );
+};
 
 ButtonComponent.defaultProps = {
   children: null,
-  onClick: () => {}
+  onClick: () => {},
+  color: '#000',
 };
--- src/app/button/button.stories.tsx
+++ src/app/button/button.stories.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from '@storybook/addon-a11y';
+import { withKnobs, text, boolean, number, color } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 import { ButtonComponent } from './button.component';
 
 storiesOf('Button', module)
   .addDecorator(checkA11y)
-  .add('with text', () => <ButtonComponent>Hello Button</ButtonComponent>)
-  .add('with some emoji', () => <ButtonComponent>🦕 🦖</ButtonComponent>);
+  .addDecorator(withKnobs)
+  .add('with disable', () => (
+    <ButtonComponent disabled={boolean('Disabled', false)}>Hello Button</ButtonComponent>
+  ))
+  .add('with color', () => {
+    const value = color('Color', '#ff00ff');
+
+    return <ButtonComponent color={value}>Hello Button</ButtonComponent>;
+  })
+  .add('with dynamic text', () => {
+    const name = text('Name', ' 🦕 🦖');
+    const age = number('Age', 89);
+
+    const content = `I am ${name} and I'm ${age} years old.`;
+    return <ButtonComponent handleClick={action('button-clicked')}>{content}</ButtonComponent>;
+  });
